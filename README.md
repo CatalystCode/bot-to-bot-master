8a108f1b56b69c2c29597a99e35dc05aee9de7ba
amitu <ami.turgman@gmail.com>
2017-03-02 15:31:15 +0200

    adding proactive messages outside of the context of the dialog

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Bot-To-Bot Master
 
+This is an example for a master bot that can route user messages to other external bots (slaves) that are developed and deployed independently.
+
+## How to use
+
+* Follow these commands to download and run te master bot locally:
+
+```
+	git clone https://github.com/CatalystCode/bot-to-bot-master.git
+	cd bot-to-bot-master
+	npm install
+	npm start
+```
+
+* Run your slave bot locally on port 3978, or use the [following sample slave bot](https://github.com/CatalystCode/bot-to-bot-sub) for the demo.
+
+* In order to start conversation with this bot, open the emulator, and use the following address: `http://localhost:3977/api/messages` without providing the App Id and Password.
+
+* Type anything to start talking to the master bot. You'll get in response the step number you're in.
+
+* In order to start routing messages to your slave bot, type `slave`. Since that moment, your messages will be routed to your slave bot. 
+
+* To stop routing the messages to the slave bot and return control to the master bot, type `bye`. You should see that you got back to the next step in the dialog of the master bot.
+
+* if you type `slave` again, you'll continue from the last step that you left the slave bot before resuming control to the master bot.
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var fs = require('fs');
 var bodyParser = require('body-parser')
 var request = require('request');
 
-
-//var subBotEndpoint = 'https://105fab98.ngrok.io';
+// TODO: take from configuration
 var subBotEndpoint = 'http://localhost:3978';
 
 var port = process.env.PORT || 3977;
@@ -34,10 +33,23 @@ var bot = new builder.UniversalBot(connector, {
   storage: new builder.MemoryBotStorage()
 });
 
-var i = 0;
+var conversations = {};
+
+// Implementation the main dialog of the master bot
+// Currently just counting step number to demonstrate where we're at the conversation
 bot.dialog('/', [
   (session, args, next) => {
-    builder.Prompts.text(session, `** -MasterBot- **: step ${i++}, enter something....`);
+
+    if (!conversations[session.message.address.conversation.id]) {
+      conversations[session.message.address.conversation.id] = { address: session.message.address };
+    }
+
+    // keep track of the user's steps in the dialog
+    if (!session.privateConversationData.step) {
+      session.privateConversationData.step = 1;
+    }
+
+    builder.Prompts.text(session, `** -MasterBot- **: step ${session.privateConversationData.step++}, enter something....`);
   },
   (session, args, next) => {
     //session.send(`you typed ${args.response}`);
@@ -45,7 +57,6 @@ bot.dialog('/', [
   }
 ]);
 
-var conversationRoutes = {};
 
 app.use('/api/messages', bodyParser.json());
 
@@ -57,34 +68,48 @@ app.post('/api/messages', (req, res, next) => {
   console.log(`headers:\n${JSON.stringify(req.headers, true, 2)}\n\n`);
   console.log(`body:\n${JSON.stringify(req.body, true, 2)}`);
 
+  var userConversation = conversations[req.body.conversation.id] || {};
+
   // if in the middle of being redirected to the sub bot and the user types bye,
   // we will stop redirecting messages to the sub bot and continue with the master bot
-  if (req.body.text.startsWith('bye') && conversationRoutes[req.body.conversation.id]) {
-    delete conversationRoutes[req.body.conversation.id];
+  if (req.body.text.startsWith('bye') && userConversation.redirect) {
+     bot.send(new builder.Message()
+        .address(userConversation.address)
+        .text(`_stopping redirecting messages from slave bot: ${subBotEndpoint}_`));
+    delete userConversation.redirect;
   }
 
   // some condition to identify that we want to start routing requests
   // or the user conversation was already started being redirected to a sub-bot
-  if (req.body.text.startsWith('sub') || conversationRoutes[req.body.conversation.id]) {
+  if (req.body.text.startsWith('slave') || userConversation.redirect) {
 
-    // store somewhere the fact that from now on we need to route this specific user
-    // messages to a specific sub-bot. For now storing in-memory
-    conversationRoutes[req.body.conversation.id] = {
-      redirect: {
+    // store the fact that from now on we need to route this specific user
+    // messages to a specific sub-bot.
+    if (!userConversation.redirect) {
+      userConversation.redirect = {
         url: subBotEndpoint + '/api/messages/'
-      }
-    };
+      };
+
+       bot.send(new builder.Message()
+        .address(userConversation.address)
+        .text(`_starting redirecting messages to slave bot: ${subBotEndpoint}_`));
+    }
 
     return request({
-      uri: conversationRoutes[req.body.conversation.id].redirect.url,
+      uri: userConversation.redirect.url,
       method: 'POST',
       headers: req.headers,
       json: true,
       body: req.body
     }, (err, subBotRes) => {
       if (err) {
-        console.error(`error from sub bot: ${err}`);
+        console.error(`error from slave bot: ${err}`);
+
         // use the botbuilder to send an error message to the user using his address
+        bot.send(new builder.Message()
+          .address(userConversation.address)
+          .text("Sorry, there was an error in the slave bot...!"));
+
         return res.end();
       }
 
@@ -105,7 +130,5 @@ app.post('/api/messages', connector.listen());
 app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
-
-
 
 module.exports = app;
